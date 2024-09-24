9eebf1e46a2d664faa818634b4cae3295e2d40ee
KerndBloOs <vanDante@gmx.de>
2024-09-24 21:29:08 +0200

    change tmux and session options in neovim

--- nvim/.config/nvim/lua/bloosvim/core/options.lua
+++ nvim/.config/nvim/lua/bloosvim/core/options.lua
@@ -30,6 +30,9 @@ opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or 
 -- clipboard
 opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
+-- sessions
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
